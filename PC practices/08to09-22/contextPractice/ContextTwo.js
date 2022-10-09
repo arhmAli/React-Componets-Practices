9895c88bfe1937e0821e915fe678c8397dd92501
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-10 00:05:10 +0500

    added context two

---
+++ PC practices/08to09-22/contextPractice/ContextTwo.js
@@ -0,0 +1,10 @@
+
+import React,{ useContext, useEffect ,useState } from 'react'
+import ContextThree from './ContextThree'
+
+ const ContextTwo=()=> {
+  return (
+    <div><ContextThree/></div>
+  )
+}
+export default ContextTwo
