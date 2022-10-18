d46b9cc6ed1c593f313663b76309edd8e9baf148
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-18 05:38:23 +0500

    added usestate

---
+++ 10 2022/useState/SomePract.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+const SomePract = () => {
+    let [name, setName] = useState({
+        firstName: "something",
+        lastName: "somethingElse"
+    });
+    return (
+        <>
+            <div onClick={() => setName({
+                firstName: "update",
+                lastName: "updateTwos"
+            })}>
+                {name.firstName + name.lastName}
+
+            </div>
+        </>
+    );
+};
+
+export default SomePract;
