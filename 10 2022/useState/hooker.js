da1d16c37ae6175eb1c921d6a6ddf8009e0f4e97
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-20 16:18:18 +0500

    created useeffect

---
+++ 10 2022/useState/hooker.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(1)
+  const [dec,setdec]=useState(1)
+  // function handleChange() {
+  //   setCount((prevCount) => {
+  //     return { ...prevCount, count: prevCount + 1 };
+  //   });
+  // }
+  useEffect(()=>{
+    dec===0?alert("YOU Lose"):null
+  },[dec])
+  return (
+    <div className="App">
+      <h1>{count}-{dec}</h1>
+      <button onClick={()=>setCount(count+1)}>Click to change</button>
+    <button onClick={()=>setdec(dec-1)}>Click</button>
+    </div>
+  );
+}
