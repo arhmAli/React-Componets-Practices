4c2e2e3b4f7eaaa00a12e28e1caf00027523fdc8
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-22 05:06:37 +0500

    created RPS

---
+++ 10 2022/useState/Game.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./styles.css";
+export default function App() {
+  const [draw, setDraw] = useState(0, {
+    value: [1]
+  });
+  let rand = ["rock", "paper", "scissors", "Shoot"];
+  let here = Math.floor(Math.random() * rand.length);
+  let here_1 = Math.floor(Math.random() * rand.length);
+
+  function randomizer() {
+    console.log(here);
+    setDraw((draw.value = here));
+    console.log(draw.value);
+  }
+  return (
+    <div className="App">
+      <h1>Rock Paper Scissors</h1>
+      <h3>{`You got ${rand[here]} vs ${rand[here_1]}`}</h3>
+      <button onClick={randomizer}>Click to draw</button>
+    </div>
+  );
+}
