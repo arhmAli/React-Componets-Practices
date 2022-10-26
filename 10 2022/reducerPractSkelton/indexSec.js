c2660b17fc8f7e583e9903a5cf6df7fd0b6d41dd
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-26 13:13:05 +0500

    create

---
+++ 10 2022/reducerPractSkelton/indexSec.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
