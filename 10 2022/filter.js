239885e9409c7bf2be71e5a7083e0939d0031b09
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-05 21:30:47 +0500

    created filter

---
+++ 10 2022/filter.js
@@ -0,0 +1,11 @@
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {
+  let positive=[]
+  arr.forEach((item)=>item>=0?positive.push(item):null)
+  let sum=positive.reduce((a,b)=>a+b,0)
+  return sum
+  }
