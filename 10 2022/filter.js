09a418dbba04644c0ed2668134c95b48199c87bd
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-05 21:32:05 +0500

    created filtering in array

--- 10 2022/filter.js
+++ 10 2022/filter.js
@@ -1,8 +1,10 @@
-You get an array of numbers, return the sum of all of the positives ones.
+// You get an array of numbers, return the sum of all of the positives ones.
 
-Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
-Note: if there is nothing to sum, the sum is default to 0.
+// Note: if there is nothing to sum, the sum is default to 0.
+
+//This example can be used in react too for filtering of some values in an array that are positive 
 function positiveSum(arr) {
   let positive=[]
   arr.forEach((item)=>item>=0?positive.push(item):null)
