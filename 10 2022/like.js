c705ec41500b3c4575f9a68085fbe83fd8b2950c
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-08 11:54:35 +0500

    made simple UI updating component

--- 10 2022/like.js
+++ 10 2022/like.js
@@ -3,8 +3,13 @@ import {useState} from 'react'
 
 const like=()=>{
   const[like,setLike]=useState(0);
+  
+  const handleClick=()=>{
+  setLike(like+1)
+  }
   return(
-  <h1>YOUR FACEBOOK LIKES!</h1>
-    <button>Like!</button>
+  <h1>`YOUR FACEBOOK LIKES!${like}` </h1>
+    
+    <button onClick={handleClick()}>Like!</button>
   )
 }
