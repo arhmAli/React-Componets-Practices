175f9154705fab45848b5041a27738017a6236d8
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-12 01:25:58 +0500

    added main logic

---
+++ Laptop Practices/2022-2023/Lifting-up-state/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import List from "./List";
+import Search from "./Search";
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Search /> */}
+      <List />
+    </div>
+  );
+}
