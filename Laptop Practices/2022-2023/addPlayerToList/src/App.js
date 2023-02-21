001544511291223f8d07ef5ec851f30554187e31
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-21 07:29:46 +0500

    created App

---
+++ Laptop Practices/2022-2023/addPlayerToList/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+//import List from "./List";
+//import Search from "./Search";
+import Create from "./Create";
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Search /> */}
+      {/* <List /> */}
+      <Create />
+    </div>
+  );
+}
